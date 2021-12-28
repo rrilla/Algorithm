a44d1f04b672523cbbdbf17a925951db0b494717
rrilla <rrilla@naver.com>
2021-12-28 23:53:14 +0900

    feat: 해쉬함수 - 마라톤 풀이

--- 피보나치수열.js
+++ 피보나치수열.js
@@ -2,31 +2,31 @@
 // // n번째 값을 출력하라.
 
 // 반복문 미사용
-// const fibonacci = (n) => {
-//   const newArr = [0, 1];
-//   const fib = (n) => {
-//     if (newArr[n] !== undefined) {
-//       return newArr[n];
-//     }
-//     newArr[n] = fib(n - 1) + fib(n - 2);
-//     return newArr[n];
-//   };
-
-//   console.log(fib(n));
-// }
-
-// 반복문 사용
 const fibonacci = (n) => {
   const arr = [0, 1];
-  if (n <= 2) {
-    console.log(arr[n - 1]);
-    return;
-  }
-  for (let i = 2; i < n; i++) {
-    arr.push(arr[i - 2] + arr[i - 1]);
-  }
-  console.log(arr[n - 1]);
+  const fib = (n) => {
+    if (arr[n] !== undefined) {
+      return arr[n];
+    }
+    arr[n] = fib(n - 1) + fib(n - 2);
+    return arr[n];
+  };
+
+  console.log(fib(n));
 };
+
+// 반복문 사용
+// const fibonacci = (n) => {
+//   const arr = [0, 1];
+//   if (n <= 2) {
+//     console.log(arr[n - 1]);
+//     return;
+//   }
+//   for (let i = 2; i < n; i++) {
+//     arr.push(arr[i - 2] + arr[i - 1]);
+//   }
+//   console.log(arr[n - 1]);
+// };
 
 fibonacci(1);
 fibonacci(2);
