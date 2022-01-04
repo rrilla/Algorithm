32f71288e36fd5d84b3e38f8cd288aced65c49f3
rrilla <rrilla@naver.com>
2022-01-04 23:20:08 +0900

    정렬 - H-Index 풀이

--- H-Index.js
+++ H-Index.js
@@ -17,6 +17,10 @@
 // 출처 프로그래머스
 
 function solution(citations) {
-  var answer = 0;
-  return answer;
+  citations = citations.sort((a, b) => b - a);
+  let i = 0;
+  while (i + 1 <= citations[i]) {
+    i++;
+  }
+  return i;
 }
